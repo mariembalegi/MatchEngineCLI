f53708307ec0c3c99e640e6371831bf05f6db387
amal-smairi <amal.smairi@etudiant-enit.utm.tn>
2025-05-07 19:55:57 +0100

    amal updates

---
+++ src/MatchEngineCLI/SelectionneurSansResultat.java
@@ -0,0 +1,11 @@
+import java.util.List;
+
+public class SelectionneurSansResultat implements Selectionneur {
+
+    @Override
+    public List<List<Object>> selectionner(List<List<Object>> resultatsAvecScore) {
+        return resultatsAvecScore;
+    }
+}
+
+
