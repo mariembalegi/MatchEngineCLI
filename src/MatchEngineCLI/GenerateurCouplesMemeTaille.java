d85b953f6050727c545e80bddafc8f9b7743d09e
maysa-khalsi <maysa.khalsi@etudiant-enit.utm.tn>
2025-05-08 19:24:01 +0100

    com

---
+++ src/MatchEngineCLI/GenerateurCouplesMemeTaille.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class GenerateurCouplesMemeTaille implements GenerateurDeCandidats {
+
+    @Override
+    public List<CoupleDeNoms> generer(List<Nom> liste1, List<Nom> liste2) {
+        Map<Integer, List<Nom>> tailleANoms = new HashMap<>();
+        for (Nom nom : liste2) {
+            int taille = nom.toString().length();
+            if (!tailleANoms.containsKey(taille)) {
+            tailleANoms.put(taille, new ArrayList<>());
+        }
+        tailleANoms.get(taille).add(nom);
+        }
+
+        List<CoupleDeNoms> couples = new ArrayList<>();
+        for (Nom nom1 : liste1) {
+            int taille = nom1.toString().length();
+            if (tailleANoms.containsKey(taille)) {
+                for (Nom nom2 : tailleANoms.get(taille)) {
+                    couples.add(new CoupleDeNoms(nom1, nom2));
+                }
+            }
+        }
+
+        return couples;
+    }
+}
