9dd128c53286b17878790fdbd7c4c9bd5d134a17
maysa-khalsi <maysa.khalsi@etudiant-enit.utm.tn>
2025-05-13 16:16:15 +0100

    mm

--- src/MatchEngineCLI/SelectionneurParSeuil.java
+++ src/MatchEngineCLI/SelectionneurParSeuil.java
@@ -1,24 +1,22 @@
 package MatchEngineCLI;
 import java.util.*;
 import java.util.ArrayList;
+
 public class SelectionneurParSeuil implements Selectionneur {
     private double seuil;
+    
     public SelectionneurParSeuil(double seuil) {
         this.seuil = seuil;
-        }
-        @Override
-        public List<CoupleAvecScore> selectionner(List<CoupleAvecScore> resultatsAvecScore) {
-            List<CoupleAvecScore> resultat = new ArrayList<>();
-            for (int i = 0; i < resultatsAvecScore.size(); i++) {
-                CoupleAvecScore ligne = resultatsAvecScore.get(i);
-                double score =  ligne.score();
-                if (score >= seuil) {
-                    resultat.add(ligne);
-                }
-            }
-
-            return resultat;
-        }
     }
-
-
+    
+    @Override
+    public List<CoupleDeNoms> selectionner(List<CoupleAvecScore> resultatsAvecScore) {
+        List<CoupleDeNoms> resultat = new ArrayList<>();
+        for (CoupleAvecScore couple : resultatsAvecScore) {
+            if (couple.score() >= seuil) {
+                resultat.add(new CoupleDeNoms(couple.nom1(), couple.nom2()));
+            }
+        }
+        return resultat;
+    }
+}
