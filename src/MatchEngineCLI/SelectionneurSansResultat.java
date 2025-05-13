9a0080fc9c2fad39e4f50051834f38ba11c94347
amal-smairi <amal.smairi@etudiant-enit.utm.tn>
2025-05-13 17:08:23 +0100

    amal updates

--- src/MatchEngineCLI/SelectionneurSansResultat.java
+++ src/MatchEngineCLI/SelectionneurSansResultat.java
@@ -5,13 +5,7 @@ import java.util.List;
 public class SelectionneurSansResultat implements Selectionneur {
 
     @Override
-    public List<CoupleDeNoms> selectionner(List<CoupleAvecScore> resultatsAvecScore) {
-        List<CoupleDeNoms> resultats = new ArrayList<>();
-        
-        for (CoupleAvecScore couple : resultatsAvecScore) {
-            resultats.add(new CoupleDeNoms(couple.nom1(), couple.nom2()));
+    public List<CoupleAvecScore> selectionner(List<CoupleAvecScore> resultatsAvecScore) {
+            return resultatsAvecScore;
         }
-        
-        return resultats;
-    }
 }
