3f54a4b604ea49a9ecad76f0a45cc77d0b3c6e0b
Mariem Balegi <meriem.balegi@etudiant-enit.utm.tn>
2025-05-14 19:21:16 +0100

    test

---
+++ src/MatchEngineCLI/RecuperateurCSVTest.java
@@ -0,0 +1,31 @@
+package MatchEngineCLI;
+
+import org.junit.jupiter.api.Test;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class RecuperateurCSVTest {
+
+    @Test
+    public void testRecuperationDepuisUnFichier() {
+        // Remplace ce chemin par le chemin réel de ton fichier
+        String filePath = "C:\\Users\\mariem\\Downloads\\peps_names_200.csv";
+
+        Path path = Paths.get(filePath);
+        RecuperateurCSV recuperateur = new RecuperateurCSV(path.toString());
+
+        long startTime = System.nanoTime();
+        List<Nom> noms = recuperateur.recuperer();
+        long endTime = System.nanoTime();
+
+        long durationInMillis = (endTime - startTime) / 1_000_000;
+        System.out.println("Nombre de noms récupérés : " + noms.size());
+        System.out.println("Temps d'exécution : " + durationInMillis + " ms");
+
+        for (Nom nom : noms) {
+            System.out.println(nom);
+        }
+    }
+}
