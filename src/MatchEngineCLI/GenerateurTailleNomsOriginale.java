2b8d4136d6f3c61ecfe9b259c568ed89a34d6d64
Mariem Balegi <meriem.balegi@etudiant-enit.utm.tn>
2025-05-14 08:30:51 +0100

    maryem

---
+++ src/MatchEngineCLI/GenerateurTailleNomsOriginale.java
@@ -0,0 +1,27 @@
+package MatchEngineCLI;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GenerateurTailleNomsOriginale implements GenerateurCandidats {
+    private static final int DIFFERENCE_MAX = 2;
+    @Override
+    public List<CoupleDeNoms> generer(List<Nom> liste1, List<Nom> liste2) {
+        List<CoupleDeNoms> couples = new ArrayList<>();
+        for (Nom nom1 : liste1) {
+            String s1 = nom1.nomNonTraite();
+
+            for (Nom nom2 : liste2) {
+                String s2 = nom2.nomNonTraite();
+
+                int diff = Math.abs(s1.length() - s2.length());
+
+                if (diff <= DIFFERENCE_MAX) {
+                    couples.add(new CoupleDeNoms(nom1, nom2));
+                }
+            }
+        }
+        return couples;
+    }
+
+}
---
+++ src/MatchEngineCLI/GenerateurTailleNomsTraite.java
@@ -0,0 +1,25 @@
+package MatchEngineCLI;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class GenerateurTailleNomsTraite implements GenerateurCandidats {
+    private static final int DIFFERENCE_MAX = 1;
+
+    public List<CoupleDeNoms> generer(List<Nom> liste1, List<Nom> liste2) {
+        List<CoupleDeNoms> couples = new ArrayList<>();
+        for (Nom nom1 : liste1) {
+            int taille1 = nom1.nomTraite().size();
+
+            for (Nom nom2 : liste2) {
+                int taille2 = nom2.nomTraite().size();
+                int diff = Math.abs(taille1 - taille2);
+
+                if (diff <= DIFFERENCE_MAX) {
+                    couples.add(new CoupleDeNoms(nom1, nom2));
+                }
+            }
+        }
+        return couples;
+    }
+}
